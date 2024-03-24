b08464eaf95d535270a842ef5a77b7bd1ba58364
DoanDucBao <psawn0972055909@yahoo.com.vn>
2024-03-24 22:42:28 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AuthInputs from './components/AuthInputs'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+    <Header />
+    <main>
+      <AuthInputs />
+    </main>
+  </>
+  )
+}
+
+export default App
